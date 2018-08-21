240100862daee8a7e11f589888f23c5a774e6d69
Kwankaew Uttama <kwankaew.u@ku.th>
2018-08-21 19:34:45 +0700

    clean cell

--- src/Cell.java
+++ src/Cell.java
@@ -40,44 +40,44 @@ public class Cell {
         int count = 0;
         int cnt = 0;
         int[][] keep = new int[100][2];
-        for (int i = 1; i < size - 1; i++) {
-            for (int j = 1; j < size - 1; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 count = checkCycle(i, j, n);
                 //System.out.printf("count[%d][%d] = %d\n", i, j, count);
-                if (count == 3) {
-                    if(!isLife(n[i][j])) {
+                if (count == 3) { //new born
+                    if (!isLife(n[i][j])) {
                         n[i][j] = -1;
                     }
-                    if (i > 1 && i < (size - 2) && j > 1 && j < (size - 2)) {
-                        System.out.printf("[%d][%d]\n", i, j);
-                        if (checkCycle(i - 1, j - 1, n) == 3 && !isLife(n[i - 1][j - 1])) {
-                            n[i - 1][j - 1] = -1;//born
-                        }
-                        if (checkCycle(i - 1, j, n) == 3 && !isLife(n[i - 1][j])) {
-                            n[i - 1][j] = -1;//born
-                        }
-                        if (checkCycle(i - 1, j + 1, n) == 3 && !isLife(n[i - 1][j + 1])) {
-                            n[i - 1][j + 1] = -1;//born
-                        }
-                        if (checkCycle(i, j - 1, n) == 3 && !isLife(n[i][j - 1])) {
-                            n[i][j - 1] = -1;//born
-                        }
-                        if (checkCycle(i, j + 1, n) == 3 && !isLife(n[i][j + 1])) {
-                            n[i][j + 1] = -1;//born
-                        }
-                        if (checkCycle(i + 1, j - 1, n) == 3 && !isLife(n[i + 1][j - 1])) {
-                            n[i + 1][j - 1] = -1;//born
-                        }
-                        if (checkCycle(i + 1, j, n) == 3 && !isLife(n[i + 1][j])) {
-                            n[i + 1][j] = -1;//born
-                        }
-                        if (checkCycle(i + 1, j + 1, n) == 3 && !isLife(n[i + 1][j + 1])) {
-                            n[i + 1][j + 1] = -1;//born
-                        }
-
+//                    if (i > 1 && i < (size - 1) && j > 1 && j < (size - 1)) {
+                    System.out.printf("[%d][%d]\n", i, j);
+                    if (checkCycle(i - 1, j - 1, n) == 3 && !isLife(n[i - 1][j - 1])) {
+                        n[i - 1][j - 1] = -1;//born
+                    }
+                    if (checkCycle(i - 1, j, n) == 3 && !isLife(n[i - 1][j])) {
+                        n[i - 1][j] = -1;//born
+                    }
+                    if (checkCycle(i - 1, j + 1, n) == 3 && !isLife(n[i - 1][j + 1])) {
+                        n[i - 1][j + 1] = -1;//born
+                    }
+                    if (checkCycle(i, j - 1, n) == 3 && !isLife(n[i][j - 1])) {
+                        n[i][j - 1] = -1;//born
+                    }
+                    if (checkCycle(i, j + 1, n) == 3 && !isLife(n[i][j + 1])) {
+                        n[i][j + 1] = -1;//born
+                    }
+                    if (checkCycle(i + 1, j - 1, n) == 3 && !isLife(n[i + 1][j - 1])) {
+                        n[i + 1][j - 1] = -1;//born
+                    }
+                    if (checkCycle(i + 1, j, n) == 3 && !isLife(n[i + 1][j])) {
+                        n[i + 1][j] = -1;//born
+                    }
+                    if (checkCycle(i + 1, j + 1, n) == 3 && !isLife(n[i + 1][j + 1])) {
+                        n[i + 1][j + 1] = -1;//born
                     }
 
-                } else if (count == 1 || count > 3) {
+//                    }
+
+                } else if (count == 1 || count > 3) { //die spot
                     keep[cnt][0] = i;
                     keep[cnt][1] = j;
                     cnt++;
@@ -86,14 +86,14 @@ public class Cell {
             }
         }
         for (int i = 0; i <= cnt; i++) {
-            n[keep[i][0]][keep[i][1]] = 0;//die
+            n[keep[i][0]][keep[i][1]] = 0;//set dead spot value
             //System.out.println("die " + keep[i][0] + " " + keep[i][1]);
         }
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 if (n[i][j] == -1) {
                     System.out.println("born " + i + " " + j);
-                    n[i][j] = 1;//born
+                    n[i][j] = 1;//set new born spot value
                 }
             }
         }
@@ -102,38 +102,46 @@ public class Cell {
 
     public int checkCycle(int i, int j, int[][] n) {
         int count = 0;
-        if (n[i - 1][j - 1] == 1) {
-            //System.out.println("[i-1][j-1]");
-            count += 1;
-        }
-        if (n[i - 1][j] == 1) {
-            //System.out.println("[i-1][j]");
-            count += 1;
-        }
-        if (n[i - 1][j + 1] == 1) {
-            //System.out.println("[i-1][j+1]");
-            count += 1;
-        }
-        if (n[i][j - 1] == 1) {
-            //System.out.println("[i][j - 1]");
-            count += 1;
-        }
-        if (n[i][j + 1] == 1) {
-            //System.out.println("[i][j + 1]");
-            count += 1;
-        }
-        if (n[i + 1][j - 1] == 1) {
-            //System.out.println("[i + 1][j - 1]");
-            count += 1;
-        }
-        if (n[i + 1][j] == 1) {
-            //System.out.println("[i + 1][j]");
-            count += 1;
-        }
-        if (n[i + 1][j + 1] == 1) {
-            //System.out.println("[i+1][j+1]");
-            count += 1;
-        }
+        if (i > 0 && j > 0)
+            if (n[i - 1][j - 1] == 1) {
+                //System.out.println("[i-1][j-1]");
+                count += 1;
+            }
+        if (i > 0)
+            if (n[i - 1][j] == 1) {
+                //System.out.println("[i-1][j]");
+                count += 1;
+            }
+        if (i > 0 && j < size - 1)
+            if (n[i - 1][j + 1] == 1) {
+                //System.out.println("[i-1][j+1]");
+                count += 1;
+            }
+        if (j > 0)
+            if (n[i][j - 1] == 1) {
+                //System.out.println("[i][j - 1]");
+                count += 1;
+            }
+        if (j < size - 1)
+            if (n[i][j + 1] == 1) {
+                //System.out.println("[i][j + 1]");
+                count += 1;
+            }
+        if (i < size - 1 && j > 0)
+            if (n[i + 1][j - 1] == 1) {
+                //System.out.println("[i + 1][j - 1]");
+                count += 1;
+            }
+        if (i < size - 1)
+            if (n[i + 1][j] == 1) {
+                //System.out.println("[i + 1][j]");
+                count += 1;
+            }
+        if (i < size - 1 && j < size - 1)
+            if (n[i + 1][j + 1] == 1) {
+                //System.out.println("[i+1][j+1]");
+                count += 1;
+            }
         return count;
     }
 }
